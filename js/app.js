3fc1f9f44b2856b07695270ff5e8002abf2881be
HJ <hongjinlau@gmail.com>
2018-12-23 14:22:25 +0800

    feat: Add event listener for click to open cards

--- js/app.js
+++ js/app.js
@@ -60,3 +60,11 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+ function openCard(event) {
+   event.target.classList.add('open', 'show');
+   console.log(event.target);
+ }
+
+ document.querySelector('.deck').addEventListener('click', openCard);
